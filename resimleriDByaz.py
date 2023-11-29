8718586abc74efb0e768fc860b684ac7a044fd62
Eren T <102971562+99eren99@users.noreply.github.com>
2023-11-29 19:51:25 +0300

    Add files via upload

--- resimleriDByaz.py
+++ resimleriDByaz.py
@@ -5,13 +5,13 @@ import pymongo
 import os
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client["xyzswqss"]#dbname
+db = client["antalyangundemgazetesiDB"]#dbname
 
 resimler=db["resimler"]
 
         
-for yearFolder in reversed(range(2018,2024)):
-    for monthFolder in reversed(range(1,13)):
+for yearFolder in range(2017,2024):
+    for monthFolder in range(1,13):
         try:
             images=os.listdir(f"./{yearFolder}/{monthFolder}/")
         except:
@@ -21,11 +21,14 @@ for yearFolder in reversed(range(2018,2024)):
         images=list(filter(lambda imgName: not "x" in imgName.split("-")[-1], images))
 
         for imageName in images:
-            print(f"./{yearFolder}/{monthFolder}/{imageName}")
-            ref=f"https://antalyaningundemi.com/wp-content/uploads/{yearFolder}/{monthFolder}/{imageName}"
-            img=Image.open(f"./{yearFolder}/{monthFolder}/{imageName}")
-            buff = BytesIO()
-            img.save(buff, format="JPEG")
-            base64img = base64.b64encode(buff.getvalue()).decode("utf-8")
-            resimDocument={"href":ref,"base64img":base64img}
-            resimler.insert_one(resimDocument)
+            try:
+                print(f"./{yearFolder}/{monthFolder}/{imageName}")
+                ref=f"https://antalyaningundemi.com/wp-content/uploads/{yearFolder}/{monthFolder}/{imageName}"
+                img=Image.open(f"./{yearFolder}/{monthFolder}/{imageName}").convert("RGB")
+                buff = BytesIO()
+                img.save(buff, format="JPEG")
+                base64img = base64.b64encode(buff.getvalue()).decode("utf-8")
+                resimDocument={"href":ref,"base64img":base64img}
+                resimler.insert_one(resimDocument)
+            except:
+                pass
