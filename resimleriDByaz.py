412f067ff1726c7f6683910dcffc4685a830334d
Eren T <102971562+99eren99@users.noreply.github.com>
2023-11-29 20:07:32 +0300

    Update resimleriDByaz.py

--- resimleriDByaz.py
+++ resimleriDByaz.py
@@ -10,15 +10,14 @@ db = client["antalyangundemgazetesiDB"]#dbname
 resimler=db["resimler"]
 
         
-for yearFolder in range(2017,2024):
-    for monthFolder in range(1,13):
+for yearFolder in reversed(range(2023,2024)):
+    for monthFolder in reversed(range(1,13)):
         try:
             images=os.listdir(f"./{yearFolder}/{monthFolder}/")
         except:
             continue
 
         #filter images
-        images=list(filter(lambda imgName: not "x" in imgName.split("-")[-1], images))
 
         for imageName in images:
             try:
